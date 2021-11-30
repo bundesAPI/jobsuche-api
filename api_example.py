2458f2ba1fee154d3a334ee29f660dabcd1de178
Lilith Wittmann <LilithWittmann@users.noreply.github.com>
2021-11-30 21:29:57 +0100

    add example for job details

--- api_example.py
+++ api_example.py
@@ -1,5 +1,5 @@
 import requests
-
+import base64
 def get_jwt():
     """fetch the jwt token object"""
     headers = {
@@ -19,7 +19,7 @@ def get_jwt():
 
     return response.json()
 
-def search(jwt):
+def search(jwt, what, where):
     """search for jobs. params can be found here: https://jobsuche.api.bund.dev/"""
     params = (
         ('angebotsart', '1'),
@@ -27,7 +27,8 @@ def search(jwt):
         ('pav', 'false'),
         ('size', '100'),
         ('umkreis', '25'),
-        ('was', 'bahn'),
+        ('was', what),
+        ('wo', where),
     )
 
     headers = {
@@ -42,8 +43,25 @@ def search(jwt):
     return response.json()
 
 
+def job_details(jwt, job_ref):
+
+    headers = {
+        'User-Agent': 'Jobsuche/2.9.3 (de.arbeitsagentur.jobboerse; build:1078; iOS 15.1.0) Alamofire/5.4.4',
+        'Host': 'rest.arbeitsagentur.de',
+        'OAuthAccessToken': jwt,
+        'Connection': 'keep-alive',
+    }
+
+    response = requests.get(
+        f'https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v2/jobdetails/{(base64.b64encode(job_ref.encode())).decode("UTF-8")}',
+        headers=headers, verify=False)
+
+    return response.json()
+
 
 if __name__ == "__main__":
     jwt = get_jwt()
-    print(search(jwt["access_token"]))
+    result = search(jwt["access_token"], "bahn", "berlin")
+    print(result['stellenangebote'][0]["refnr"])
+    print(job_details(jwt["access_token"], result['stellenangebote'][0]["refnr"]))
 
